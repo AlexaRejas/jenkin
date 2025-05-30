4b8fa48d6bf95a7febb683a76ba349fd1b802925
Alexa Rejas Carrasco <126887187+AlexaRejas@users.noreply.github.com>
2025-05-30 05:19:24 +0000

    v1.3

--- src/main/java/pe/edu/vallegrande/tranformacion/controller/FamilyController.java
+++ src/main/java/pe/edu/vallegrande/tranformacion/controller/FamilyController.java
@@ -36,45 +36,5 @@ public class FamilyController {
                 : ResponseEntity.status(HttpStatus.NOT_FOUND).body("Familia no encontrada con ID: " + id);
     }
 
-    @PostMapping("/create")
-    public ResponseEntity<?> createFamily(@RequestBody Family family) {
-        Family savedFamily = familyRepository.save(family);
-        return ResponseEntity.status(HttpStatus.CREATED).body(savedFamily);
-    }
-
-    @PutMapping("/actualizar/{id}")
-    public ResponseEntity<?> actualizarFamily(@PathVariable Long id, @RequestBody Family family) {
-        if (!familyRepository.existsById(id)) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Familia no encontrada con ID: " + id);
-        }
-
-        family.setId(id);
-        Family updatedFamily = familyService.guardarFamily(family);
-        return ResponseEntity.ok(updatedFamily);
-    }
-
-    @PutMapping("/restore/{id}")
-    public ResponseEntity<?> restoreFamily(@PathVariable Long id) {
-        try {
-            Family restoredFamily = familyService.restoreFamily(id);
-            return restoredFamily != null ? ResponseEntity.ok(restoredFamily)
-                    : ResponseEntity.status(HttpStatus.NOT_FOUND).body("Familia no encontrada para restaurar con ID: " + id);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al restaurar la familia: " + e.getMessage());
-        }
-    }
-
-    @PutMapping("/eliminar-logico/{id}")
-    public ResponseEntity<?> eliminarLogico(@PathVariable Long id) {
-        Family family = familyRepository.findById(id).orElse(null);
-        if (family == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Familia no encontrada con ID: " + id);
-        }
-
-        // Cambiar el estado a "I" (inactivo) en lugar de eliminar
-        // Aquí puedes cambiar el estado si es necesario
-        familyRepository.save(family);
-
-        return ResponseEntity.ok("Familia marcada como inactiva correctamente.");
-    }
+    
 }
